3254af9bb86739cfc29403046278e149b11b835b
mariagepfrie-ship-it <maria.gepfrie@escola.pr.gov.br>
2026-06-10 15:12:15 -0300

    Update script.js

--- img/script.js
+++ img/script.js
@@ -1,1 +1,21 @@
 
+function mostrarInfo(){
+    document.getElementById("info").style.display = "block";
+}
+
+function iniciarContador(){
+
+    let litros = 0;
+
+    const contador = setInterval(() => {
+
+        litros += 25;
+
+        document.getElementById("litros").textContent = litros;
+
+        if(litros >= 1000){
+            clearInterval(contador);
+        }
+
+    }, 100);
+}
