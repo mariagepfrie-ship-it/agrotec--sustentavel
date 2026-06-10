ca10c57da2952d304c11d266ae408ec1859efa15
mariagepfrie-ship-it <maria.gepfrie@escola.pr.gov.br>
2026-06-10 14:43:24 -0300

    Update script.js

--- img/script.js
+++ img/script.js
@@ -1,10 +1,1 @@
-function mostrarMensagem() {
-    alert("O Agro Forte acredita que a produção sustentável é o caminho para o futuro!");
-}
 
-let contador = 0;
-
-function plantarArvore() {
-    contador++;
-    document.getElementById("arvores").innerText = contador;
-}
